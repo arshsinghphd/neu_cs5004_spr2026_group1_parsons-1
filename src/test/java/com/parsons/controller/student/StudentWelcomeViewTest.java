a0b63c78e13334f11d210a5f20b4a1060824bba8
arshsinghphd <57355255+arshsinghphd@users.noreply.github.com>
2026-04-08 18:37:25 -0700

    refactor StudentWelcomeView to only take service

--- src/test/java/com/parsons/controller/student/StudentWelcomeViewTest.java
+++ src/test/java/com/parsons/controller/student/StudentWelcomeViewTest.java
@@ -1,5 +1,4 @@
 package com.parsons.controller.student;
-import com.parsons.controller.DummyData;
 import com.parsons.repository.XmlParsonsProblemsRepository;
 import com.parsons.service.ParsonsProblemsService;
 
@@ -11,7 +10,7 @@ public class StudentWelcomeViewTest {
     private static final ParsonsProblemsService service = new ParsonsProblemsService(repository);
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() ->
-                new StudentWelcomeView(service.getAllProblems(), service)
+                new StudentWelcomeView(service)
         );
     }
 }
